8ced9c624e023c9c7d742cbe709acfda461acd0b
Hassanaqve <hassanaqve@gmail.com>
2022-03-17 14:37:41 +0500

    testing

--- contracts/AjToken.sol
+++ contracts/AjToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract AJToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("AJToken", "AJ") {
+    constructor(uint256 initialSupply) ERC20("AJToken1", "AJ") {
         _mint(msg.sender, initialSupply);
     }
 }
